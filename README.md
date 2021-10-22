7fc0b32d11b2c23db8f4fc84b3e7c7cbde27c64c
Nshell <nshellb@gmail.com>
2021-10-22 11:32:24 +0900

    10222021 Vue.js 상태관리 - 각 part 설명

--- README.md
+++ README.md
@@ -1,19 +1,55 @@
-# vuex_chat
+Vue.js 상태관리 - 각 part 설명
 
-## Project setup
-```
-npm install
-```
+App.vue
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+읽지 않은 메시지 알림이 각 행에 부여되고
+상단바 알림창에 모두 합한 값의 알림이 출력된다.
 
-### Compiles and minifies for production
-```
-npm run build
-```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+
+전체 message count 값은 computed 로 Header 에 표시하도록 넘겨준다.
+
+ChatList 에는 ChatList 배열을 넘겨주며
+각 항목이 클릭했을 때 발생하는 이벤트는 readChatItem 이라는 method 와 연결해준다.
+
+
+
+Header.vue
+
+App.vue 에서 넘겨받은 전체 message count 값을 출력
+props 로 넘겨받은값을 사용했다.
+
+
+[프로젝트간 유기적으로 임시적인 설정값등을 짧은 경로간에 넘겨주는 경우에는 props 를 사용하고
+vuex 를 사용하면 전역적인 범위에서 어디서든 값을 가져올수 있는것 같다.]
+
+
+
+ChatList.vue
+
+App.vue 에서 넘겨 받은 chatList 배열을 가지고 v-for 구문을 돌려서 ChatListItem componet 를 출력해준다.
+
+
+
+ChatListItem.vue
+
+ChatList.vue 에서 넘겨받은 lastmessage 와 읽지 않은 chat count 를 button 에 출력한다.
+button 의 click event 발생시 itemClick 이벤트를 발생시켜서 'click' 이라는 component click event 를 발생시킨다.
+(ChatList 에 선언된 ChatListItem @click="itemClick" 을 호출.)
+
+ChatListItem.vue 의 click event 인 'click' 으로 
+ChatList.vue 의 click event 인 'itemClick' 을 호출하고
+App.vue 의 click event 인 'read-item' 을 호출한다.
+
+
+
+App.vue의 Data 를 Header.vue 와 ChatList.vue 에 props 로 각각 전달하고
+ChatList.vue 에서 props 로 전달받은 Data 는 다시 ChatListItem.vue 에 props 로 전달한다.
+
+message count 를 App.vue 에서 각 componet 에 동기화 되는 구조.
+그러나 componet 가 추가될때 마다 App.vue 에서 동기화 하기위한 로직이 추가되어야 하는 번거로움이 있다.
+
+
+
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,65 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header
+      :new-message-count="newMessageCount"
+    />
+    <div class="container mx-auto mt-5">
+      <ChatList
+        :chat-list="chatList"
+        @read-item="readChatItem"
+      />
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue'
+import ChatList from '@/components/ChatList.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      chatList: [ // chatList 임의 지정
+        {
+          id: 1, // chat 식별 번호
+          lastMessage: '채팅 메시지1', // 가장 마지막으로 전달 받은 채팅 메시지
+          new: 1, // 읽지 않은 채팅 메시지 개수 count
+        },
+        {
+          id: 2,
+          lastMessage: '채팅 메시지2',
+          new: 2,
+        },
+        {
+          id: 3,
+          lastMessage: '채팅 메시지3',
+          new: 1,
+        },
+        {
+          id: 4,
+          lastMessage: '채팅 메시지4',
+          new: 5,
+        },
+      ]
+    };
+  },
+  computed: {
+    newMessageCount() { // 전체 count 계산 method
+      return this.chatList.map(item => item.new).reduce((a, b) => a + b); // App.vue 에 선언된 chatList 에서 item => item.new 값만 가져온뒤 reduce(더하기) 연산
+    }
+  },
+  methods: {
+    readChatItem(chatItem) { // 각 항목에 click event 를 받아서 count 를 0으로 만들어주는 method
+      this.chatList.filter(item => item.id === chatItem.id)[0].new = 0; // 클릭한 item 값의 id 값을 찾아서 클릭한 항목의 알림 count 를 0으로 만든다.
+    }
+  },
+  components: {
+    Header,
+    ChatList,
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
